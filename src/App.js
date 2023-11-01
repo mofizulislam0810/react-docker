e5a7e86d9c1cbabdab7d98eb534fdb3580ba95eb
mofizulislam0810 <mofizul.islam0810@gmail.com>
2023-11-01 12:01:33 +0600

    api call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const query = `
+  query{
+    allPerson{
+        name
+    }
+}
+`;
+
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://localhost:7113/graphql/", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    })
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData.data.allPerson,"responseData+++++++")
+        setData(responseData.data.allPerson);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setData(null);
+        setLoading(false);
+        setError(err.message);
+      });
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Docker React
+        {/* <p>
+          Hello Docker React Api
         </p>
         <p>Islam</p>
         <a
@@ -17,7 +56,10 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React And CI/CD And Docker
-        </a>
+        </a> */}
+        {
+          data?.map((item,idx)=> <><p>{item.name}</p><br></br></>)
+        }
       </header>
     </div>
   );
