46632e97f00043d8ba69148eb1294565b780f04a
mofizulislam0810 <mofizul.islam0810@gmail.com>
2023-11-07 11:00:12 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,45 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
 
 function App() {
-
-  const query = `
-  query{
-    allPerson{
-        name
-    }
-}
-`;
-
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  // useEffect(() => {
-  //   fetch("https://localhost:7113/graphql/", {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify({ query }),
-  //   })
-  //     .then((response) => response.json())
-  //     .then((responseData) => {
-  //       console.log(responseData.data.allPerson,"responseData+++++++")
-  //       setData(responseData.data.allPerson);
-  //       setLoading(false);
-  //       setError(null);
-  //     })
-  //     .catch((err) => {
-  //       setData(null);
-  //       setLoading(false);
-  //       setError(err.message);
-  //     });
-  // }, []);
-
-  // if (loading) return <p>Loading...</p>;
-  // if (error) return <p>Error: {error}</p>;
 
   return (
     <div className="App">
